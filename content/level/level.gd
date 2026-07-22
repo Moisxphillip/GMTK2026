f3adc2c49cdc15bae4323cfa9c5c68c78a44501a
Moisés Felipe Lima/Spiritw <39066875+Moisxphillip@users.noreply.github.com>
2026-07-22 14:50:31 -0300

    Added basic scenes for tests

---
+++ content/level/level.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+var score = 0
+var killCount = 0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    score = 0
+    killCount = 0
+
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+
+    pass

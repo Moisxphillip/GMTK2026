8776f47794a3abd7b70f22c7211c6166730a740a
gustavo-tomas <guga.tomas10@gmail.com>
2026-07-26 12:49:55 -0300

    Added fruit basket

--- content/level/level.gd
+++ content/level/level.gd
@@ -4,7 +4,7 @@ const levelSize = 20
 
 var fruit: Resource = preload("res://content/item/item.tscn")
 var gas: Resource = preload("res://content/env_models/gas.tscn")
-
+@onready var player = $Player
 
 var fruitCount = 0
 @onready var fruitLabel = get_node("HUD").get_node("Fruits")
@@ -14,32 +14,33 @@ var fruitTimer: Timer = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    fruitCount = 0
-    gasTimer = $GasTimer
-    fruitTimer = $FruitTimer
-    fruitLabel.text = "Fruits: " + str(fruitCount)
+	fruitCount = 0
+	gasTimer = $GasTimer
+	fruitTimer = $FruitTimer
+	fruitLabel.text = "Fruits: " + str(fruitCount)
 
 
 func add_fruit() -> void:
-    fruitCount += 1
-    fruitLabel.text = "Fruits: " + str(fruitCount)
+	fruitCount += 1
+	fruitLabel.text = "Fruits: " + str(fruitCount)
+	player.add_fruit()
 
 
 func _on_gas_timer_timeout() -> void:
-    gasTimer.start()
-    spawnItem(gas)
+	gasTimer.start()
+	spawnItem(gas)
 
 
 func _on_fruit_timer_timeout() -> void:
-    fruitTimer.start()
-    spawnItem(fruit)
+	fruitTimer.start()
+	spawnItem(fruit)
 
 
 func spawnItem(resource: Resource) -> void:
 
-    var item: Node3D = resource.instantiate()
+	var item: Node3D = resource.instantiate()
 
-    item.position.x = randi_range(-levelSize, levelSize);
-    item.position.z = randi_range(-levelSize, levelSize);
+	item.position.x = randi_range(-levelSize, levelSize);
+	item.position.z = randi_range(-levelSize, levelSize);
 
-    add_child(item)
+	add_child(item)
--- content/player/player.gd
+++ content/player/player.gd
@@ -1,33 +1,58 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var fuel:float = 15.0
+var fruitOnBasket: int = 0
+
+var chirimoya = preload("res://content/item/chirimoya.glb")
+var maracuja = preload("res://content/item/maracuja.glb")
+
 @onready var mesh = $Mesh
 @onready var fuelLabel = get_parent().get_node("HUD").get_node("Fuel")
+@onready var basket = mesh.get_node("Basket")
 
 func _ready() -> void:
-    fuelLabel.text = "Fuel: " + str(snapped(fuel, 0.1))
+	fuelLabel.text = "Fuel: " + str(snapped(fuel, 0.1))
+	pass
 
 func _physics_process(delta: float) -> void:
-    if not is_on_floor():
-        velocity += get_gravity() * delta
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
-    # if Input.is_action_just_pressed("Jump") and is_on_floor():
-    #     velocity.y = JUMP_VELOCITY
+	# if Input.is_action_just_pressed("Jump") and is_on_floor():
+	#     velocity.y = JUMP_VELOCITY
 
-    var input_dir := Input.get_vector("Left", "Right", "Up", "Down")
-    var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-    if direction and fuel > 0:
-        velocity.x = direction.x * SPEED
-        velocity.z = direction.z * SPEED
-        mesh.rotation.y = lerp_angle(mesh.rotation.y, atan2(direction.x, direction.z), 0.1)
-        fuel -= delta
-        fuelLabel.text = "Fuel: " + str(snapped(fuel, 0.1))
-    else:
-        velocity.x = move_toward(velocity.x, 0, SPEED)
-        velocity.z = move_toward(velocity.z, 0, SPEED)
-        
+	var input_dir := Input.get_vector("Left", "Right", "Up", "Down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction and fuel > 0:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		mesh.rotation.y = lerp_angle(mesh.rotation.y, atan2(direction.x, direction.z), 0.1)
+		fuel -= delta
+		fuelLabel.text = "Fuel: " + str(snapped(fuel, 0.1))
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
 
-    move_and_slide()
+	move_and_slide()
+
+func add_fruit():
+	if fruitOnBasket >= 2:
+		return
+
+	var fruit
+
+	if fruitOnBasket == 0:
+		fruit = chirimoya.instantiate()
+		fruit.position = Vector3(0.22, 0, 0.25);
+	
+	elif fruitOnBasket == 1:
+		fruit = maracuja.instantiate()
+		fruit.position = Vector3(-0.2, 0, 0);
+
+	basket.add_child(fruit)
+	fruitOnBasket += 1;
+
+	return
